a8d3b3fa433361b1c88cf39ced5986524b8d8fe3
Rajesh <rajeshkannang87@gmail.com>
2019-02-10 10:01:36 -0500

    OKTA Sample Rest Security app

---
+++ okta/src/main/java/com/rajesh/security/okta/OktaApplication.java
@@ -0,0 +1,14 @@
+package com.rajesh.security.okta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OktaApplication.class, args);
+	}
+
+}
+
